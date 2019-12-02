5447fd416d619781380205cfaabfdfcc46e2c085
Felipe Tadeu Costa Ribeiro <felipe.ribeiro@ihm.local>
2019-12-02 19:38:03 -0300

    Initial Templates and urls

--- simpleProject/simpleProject/urls.py
+++ simpleProject/simpleProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .core import views
 urlpatterns = [
+    # path('simpleProject.core.views.home',name='home'),
+    path('kleitin',views.home),
     path('admin/', admin.site.urls),
 ]
